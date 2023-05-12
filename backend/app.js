c8d16bc94a55d2d789736bc78f4f82ccee857fdc
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-05-12 12:58:57 -0500

    working with backend

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,20 @@ const port = 3000;
 const router = express.Router();
 
 app.use(express.static(path.join(__dirname, 'frontend')));
+app.use('/v1', router);
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   //app.use(express.static(path.join(__dirname, 'frontend')));
   //res.sendFile(path.join(__dirname+'/frontend/index.html'));
 
   res.send('Hello World!');
-})
+});
 
 //about me endpoint
-app.post('/about-me', function(req, res) {
-  
-})
+router.get('/about-me', function(req, res) {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
